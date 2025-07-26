d2dbc7ce8787131b9fcdd5933f4eec20a2f00715
Masensen <172938641+m4sensen@users.noreply.github.com>
2025-07-26 05:26:15 -0400

    Removing redirection

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://github.com/m4sensen/masensen-api",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
